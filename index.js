e2eec499960ac215d637ad59a0e4b0a8a00d8763
Kelvin Chijioke <ckelvin196@gmail.com>
2026-02-02 21:38:51 +0100

    fix: remove trailing slash from allowedOrigins for ajamart.store

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const mongoUrl = process.env.MONGODB_URL;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://www.ajamart.store/",
+  "https://www.ajamart.store",
   process.env.FRONTEND_URL, 
   process.env.DASHBOARD_URL,
   "https://ajah-eight.vercel.app",
